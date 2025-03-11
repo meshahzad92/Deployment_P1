0084254ec10f47158112554b48bce4ac47f5cf09
unknown <mshahzadwaris92@gmail.com>
2025-03-12 00:27:10 +0500

    Fix  issue for Railway deployment

--- database.py
+++ database.py
@@ -1,9 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 import os 
-# DATABASE_URL = "postgresql://postgres:1234@localhost:5432/task_db"
-DATABASE_URL = os.getenv("DATABASE_URL") 
+from dotenv import load_dotenv
 
+# DATABASE_URL = "postgresql://postgres:1234@localhost:5432/task_db"
+
+load_dotenv()
+
+# Get database URL
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
