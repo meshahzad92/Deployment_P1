f9a2aa1ecc2fd0c66f6e96627d91dddcf2cc0425
unknown <mshahzadwaris92@gmail.com>
2025-03-11 23:54:02 +0500

    2nd try

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-
-DATABASE_URL = "postgresql://postgres:1234@localhost:5432/task_db"
+import os 
+# DATABASE_URL = "postgresql://postgres:1234@localhost:5432/task_db"
+DATABASE_URL = os.getenv("DATABASE_URL") 
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
